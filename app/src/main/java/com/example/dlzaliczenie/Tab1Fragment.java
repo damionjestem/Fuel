7723ae5e55e4a35be2ac49bec42acc060a64008a
damionjestem <ndnd@protonmail.ch>
2019-11-06 00:42:09 +0100

    add fab

--- app/src/main/java/com/example/dlzaliczenie/Tab1Fragment.java
+++ app/src/main/java/com/example/dlzaliczenie/Tab1Fragment.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.fragment.app.Fragment;
 
@@ -19,14 +21,16 @@ public class Tab1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_one, container, false);
-        btnTEST = (Button) view.findViewById(R.id.btnTEST);
 
-        btnTEST.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "Test Button pressed", Toast.LENGTH_SHORT).show();
+                Snackbar.make(view, "Ten przycisk przywoła tworzenie nowego pojazdu", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+
 
         return view;
     }
